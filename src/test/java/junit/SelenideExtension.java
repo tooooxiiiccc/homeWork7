7a982e801c68f7956305225f90315634055cd181
Danila Morozov <danila.morozov@x5.ru>
2026-01-15 03:13:04 +0300

    Удаление несуществующего свойства driverManagerEnabled из Selenide 7.x

--- src/test/java/junit/SelenideExtension.java
+++ src/test/java/junit/SelenideExtension.java
@@ -21,36 +21,28 @@ public class SelenideExtension implements BeforeAllCallback, AfterEachCallback {
         Configuration.baseUrl = ConfigReader.getBaseUrl();
         String selenoidUrl = System.getProperty("selenoid.url");
         if (selenoidUrl != null && !selenoidUrl.isEmpty()) {
-            // Отключаем автоматическое управление драйвером для удаленного запуска
-            Configuration.driverManagerEnabled = false;
-            
-            // Настройка удаленного запуска через Selenoid
             Configuration.remote = selenoidUrl;
             Configuration.browser = System.getProperty("browser", "chrome");
             String browserVersion = System.getProperty("browser.version", "latest");
             Configuration.browserVersion = browserVersion;
             Configuration.headless = true;
             
-            // Настройка ChromeOptions для Selenoid
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--no-sandbox");
             chromeOptions.addArguments("--disable-dev-shm-usage");
             chromeOptions.addArguments("--disable-gpu");
             chromeOptions.addArguments("--window-size=1920,1080");
             
-            // Настройка Selenoid options через ChromeOptions
             HashMap<String, Object> selenoidOptions = new HashMap<>();
             selenoidOptions.put("enableVNC", true);
             selenoidOptions.put("enableVideo", false);
             selenoidOptions.put("enableLog", true);
             chromeOptions.setCapability("selenoid:options", selenoidOptions);
             
-            // Применение ChromeOptions к конфигурации
             Configuration.browserCapabilities = chromeOptions;
         } else {
             Configuration.browser = "chrome";
             Configuration.headless = false;
-            Configuration.driverManagerEnabled = true;
         }
         
         Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
